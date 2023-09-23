449e1f62725ea2e6bca2aa09c35a4eb6521a5a5f
Wizardiesha <119714634+Wizardiesha@users.noreply.github.com>
2023-09-23 12:28:02 -0400

    scrollspy function

--- Web_ACapp/HomeApp/src/views/HomeView.vue
+++ Web_ACapp/HomeApp/src/views/HomeView.vue
@@ -15,7 +15,8 @@ import { RouterLink } from 'vue-router';
             <ul class="navbar-nav ms-auto" style="color: blue;">
               <li nav-item><a class="nav-link scrollto active" href="#hero">Home</a></li>
               <li><a class="nav-link scrollto" href="#guide">Guides</a></li>
-              <li><a class="nav-link scrollto" href="#installionCalculator">Installation Calculator</a></li>
+              <li><a class="nav-link scrollto" href="#news">News</a></li>
+              <li><a class="nav-link scrollto" href="#installationCalculator">Installation Calculator</a></li>
             </ul>
           </div>
         </nav>
@@ -30,7 +31,8 @@ import { RouterLink } from 'vue-router';
             news on smart technology. We also offer community support as well as installation services to the Columbus and Dayton Ohio areas.</h5>
         </div>
       </section>
-      <div class="d-flex justify-content-center p-5">
+      <div id="guide" class="d-flex justify-content-center p-5">
+        <h2>Guides</h2>
         <div class="p-2">
           <div class="card" style="width: 18rem;">
             <img
@@ -57,7 +59,7 @@ import { RouterLink } from 'vue-router';
             </div>
           </div>
         </div>
-        <div class="p-2">
+        <div class="p-2" id="guide">
           <div class="card" style="width: 18rem;">
             <img src="https://u-tec.com/cdn/shop/t/57/assets/sence-banner.png?v=175755877485102375081689760851"
               class="card-img-top" alt="...">
@@ -70,8 +72,49 @@ import { RouterLink } from 'vue-router';
           </div>
         </div>
       </div>
-
-      <div class=" d-flex justify-content-center p-5">
+      <div id="news" class="d-flex justify-content-center p-5">
+        <h2>News</h2>
+        <div class="p-2">
+          <div class="card" style="width: 18rem;">
+            <img
+              src="https://www.the-sun.com/wp-content/uploads/sites/6/2022/09/LG-RING-DOORBELL-COMP.jpg?w=1280&quality=44"
+              class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Video Doorbell</h5>
+              <p class="card-text">Video doorbells have bad picture, amazon needs to make them better.</p>
+              <RouterLink to="../src/components/guides/DoorbellGuide.vue" class="btn btn-primary">Go somewhere
+              </RouterLink>
+            </div>
+          </div>
+        </div>
+        <div class="p-2">
+          <div class="card" style="width: 18rem;">
+            <img
+              src="https://cdn.idealo.com/folder/Product/201534/5/201534593/s3_produktbild_max/ring-alarm-kit-2nd-gen.jpg"
+              class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div class="p-2" id="guide">
+          <div class="card" style="width: 18rem;">
+            <img src="https://u-tec.com/cdn/shop/t/57/assets/sence-banner.png?v=175755877485102375081689760851"
+              class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div id="installationCalculator" class=" d-flex justify-content-center p-5">
+        <h2>Installation Calculator</h2>
         <CalculatorComponent />
       </div>
     </main>
