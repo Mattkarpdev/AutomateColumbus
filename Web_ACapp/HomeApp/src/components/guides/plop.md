2b3d59370f16b56153d3487bfb7c20de29ff1c89
Wizardiesha <119714634+Wizardiesha@users.noreply.github.com>
2023-12-18 13:04:33 -0500

    guide md function

--- Web_ACapp/HomeApp/src/components/guides/plop.md
+++ Web_ACapp/HomeApp/src/components/guides/plop.md
@@ -1,9 +1,7 @@
-# hi
+# hi A brief description of what this project does and who it's for
 
-A brief description of what this project does and who it's for
+## my name
 
-## my name is iesha
+is iesha ## Appendix Any additional information goes here.
 
-## Appendix
-
-Any additional information goes here.
+##### kjsahdkjshdlashdas;ihd'asojdSAOPjdhg/ausSFhIdhf:OJhF/HI?lfg/had;fh/lhf/ljdhF/;k
--- Web_ACapp/HomeApp/src/views/GuidesView.vue
+++ Web_ACapp/HomeApp/src/views/GuidesView.vue
@@ -4,31 +4,42 @@ import { RouterLink, RouterView } from "vue-router";
 import DoorbellGuide from "../components/guides/DoorbellGuide.vue";
 import routerGuide from "../components/GuideComponent.vue";
 import LockGuide from "../components/guides/LockGuide.vue";
-
 import { marked } from "marked";
-import VueMarkdown from "vue-markdown-render";
+import markdownit from "markdown-it";
+
 import plop from "../components/guides/plop.md";
+
+const md = markdownit();
+const result = md.render(`# Happy`);
 
 const message = ref("");
 </script>
 
 <template>
-  <body>
+  <body class="bg-transparent">
     <div class="">
       {{ routerGuide.props.routerLock
       }}{{ routerGuide.props.routerVideoDoorbell }}
       <DoorbellGuide />
-      <LockGuide />
     </div>
-    <div v-html="installationHtml"></div>
-    <VueMarkdown :source="plop" />
+    <div>wiennnnerrrrrrrrrrrrr</div>
+    <LockGuide />
   </body>
 </template>
 <script>
-const installationHtml = marked(plop);
-
 export default {
   computed: {},
 };
 </script>
-<style scoped></style>
+
+<style>
+h1 {
+  font-size: xx-large;
+}
+h2 {
+  font-size: x-large;
+}
+h4 {
+  font-size: small;
+}
+</style>
